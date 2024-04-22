a97af9d989af505c6ecb6646b9dd255aa8a718a2
RidwanAdebosin <adebosinadewale@gmail.com>
2024-04-22 23:11:22 +0100

    Ul and Li created using the document.createElement

---
+++ index.js
@@ -0,0 +1,28 @@
+const formEl= document.querySelector(".form");
+const inputEl = document.getElementById("input");
+
+
+
+// adding add event listener to catch what's written inside the input
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    toDoList()
+});
+
+// getting the ul element that has the class of .list from the HTML
+const ulEl= document.querySelector(".list")
+
+
+//creating a function that get the input value from the form
+const toDoList = (e) => {
+    //the newTodo get the value in the input
+    let newTodo =  inputEl.value;
+    //creating an li element
+    const liEl = document.createElement("li");
+    //rendering the newTodo into the liEl
+    liEl.innerText = newTodo;
+    //append the li as a child into the ulEl
+    ulEl.appendChild(liEl)
+    // after submitting the value in the input the text should clear off the input box
+    inputEl.value = ""
+}
